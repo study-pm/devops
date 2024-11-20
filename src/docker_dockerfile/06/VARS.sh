d420eb7fa5e48c4040156170a986d5281cee2b32
viktor <devpmsem@gmail.com>
2024-11-20 19:02:37 +0300

    fix(src/docker_dockerfile/06): add yellow color

--- src/docker_dockerfile/06/VARS.sh
+++ src/docker_dockerfile/06/VARS.sh
@@ -2,6 +2,7 @@
 BLUE="\e[34m"
 GREEN="\e[32m"
 RED="\e[31m"
+YELLOW="\e[33m"
 ENDCOLOR="\e[0m"
 
 # Named constants
--- src/docker_dockerfile/06/run.sh
+++ src/docker_dockerfile/06/run.sh
@@ -9,5 +9,5 @@
 
 source VARS.sh
 
-echo -e "${BLUE}Running the container${ENDCOLOR}"
+echo -e "${YELLOW}Running the container${ENDCOLOR}"
 docker run --name $1 -it "$2:${3:-latest}" pwd
