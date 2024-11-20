703d0759d7b8367004b29931dbde018e90f40894
viktor <devpmsem@gmail.com>
2024-11-20 20:58:23 +0300

    feat(src/docker_dockerfile): add lab 08

---
+++ src/docker_dockerfile/08/create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+### DESCRIPTION: Creates static resources such as Dockerfiles
+
+source VARS.sh
+
+# Writing a Dockerfile with ARG instruction
+v1="
+FROM alpine:3.9.3\n
+LABEL maintainer=\"Collabnix\"\n
+\n
+#Setting a default value to Argument WELCOME_USER\n
+ARG WELCOME_USER=Collabnix\n
+RUN echo \"Welcome \$WELCOME_USER, to Docker World!\" > message.txt\n
+CMD cat message.txt\n
+"
+
+echo -en "" $v1 | sed 's/ //' > Dockerfile
